ba6ba3796857470c50768e15323667eead7d6beb
Damilola Tolufashe <damityibk19@gmail.com>
2022-10-28 01:44:25 -0700

    Share icon created and SID added

--- src/App.js
+++ src/App.js
@@ -5,22 +5,45 @@ import profile from './img/download (3).jpeg';
 import footimg from "./img/download (1).png" ;
 import gitIcon from "./img/github-icon.svg";
 import ingressive from "./img/images (2).png";
+import share from './img/download (2).png';
 
 
 function App() {
   const links = [
-    {id: ''}
+    {id: 'btn__zuri', href: 'https://training.zuri.team/', title: 'Zuri Team'},
+    {id: 'books', href: 'https://books.zuri.team/', title: 'Zuri Books',
+      subtext: 'This is where you find books about design and coding'},
+    {id: 'book__python', href: 'https://books.zuri.team/python-for-beginners?HNG003735SGG', title: 'Python Books', subtext: 'A descriptive guide for Python beginners'},
+    {id: 'pitch', href: 'https://background.zuri.team/', title: 'Background Check for Coders',
+      subtext: 'A standalone service that does excellent background checks on developers'},
+    {id: 'book__design', href: 'https://books.zuri.team/design-rules', title: 'Design Books',
+      subtext: 'A guide to the right design principles and patterns for modern web designers'},
   ];
 
   return (
     <div className="container">
+    <div className='icon'>
+      <img id='share' src={share}/>
+      <div id='span'> 
+              <span>.</span>
+      <span>.</span>
+   <span>.</span>
+</div>
+    </div>
       <header>
         <img id="profile__img" src={profile} alt="" /> 
         <p className="name">Annette Black</p>
       </header>
       <div className="sub-container">
         <Link id="twitter" href="https://twitter.com/Ibukunoluwatbk">Twitter Link</Link>
-        <div className ='img'>        <img src={slackIcon} alt="" />
+        <div id ='slack' style ={{display: 'none'}} >
+          Damilola Blessing Tolufashe
+        </div>
+        {links.map(link => (
+          <Link key ='link.id' id={link.id} href={link.href} subtext={link.subtext}>{link.title}</Link>
+        ))}
+        <div className ='img'>        
+        <img src={slackIcon} alt="" />
         <img src={gitIcon} alt="" /></div>
 
       </div>
--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,6 @@ import '../styles/Link.css';
 
 export default function(props) {
     return (
-        <a id={props.id} className="list" href={props.href} target="_blank">{props.children}</a>
+        <a id={props.id} className="list" href={props.href} title={props.subtext} target="_blank">{props.children}</a>
     );
 }
